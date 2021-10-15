7f7fac48ff2f5cfc68e628fa2970a93a2fe5f593
DCinAC <61953469+DCinAC@users.noreply.github.com>
2021-10-15 17:09:57 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,28 @@
-let speed = 0
-let Position_LR = 0
-radio.onReceivedNumber(function (receivedNumber) {
-    speed = receivedNumber
-})
 function go_left () {
     mbit_Robot.CarCtrlSpeed2(mbit_Robot.CarState.Car_Run, speed * 0.7, 0)
 }
 function go_right () {
     mbit_Robot.CarCtrlSpeed2(mbit_Robot.CarState.Car_Run, speed, speed * 0.7)
 }
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "bot1_done") {
+        speed = 42
+    }
+})
 function foward () {
     mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, speed)
 }
+let Position_LR = 0
+let done = 0
+let speed = 0
+speed = 0
+radio.setGroup(123)
+basic.forever(function () {
+    if (mbit_Robot.Avoid_Sensor(mbit_Robot.enAvoidState.OBSTACLE) && done == 0) {
+        radio.sendString("bot2_done")
+        done = 1
+    }
+})
 basic.forever(function () {
     if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.White) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.RightState, mbit_Robot.enLineState.Black)) {
         Position_LR = 0
@@ -24,6 +35,9 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
+    basic.showString("" + (speed))
+})
+basic.forever(function () {
     if (Position_LR == 0) {
         go_right()
     } else if (Position_LR == 1) {
@@ -33,7 +47,4 @@ basic.forever(function () {
     } else {
     	
     }
-})
-basic.forever(function () {
-    basic.showString("" + (speed))
 })
